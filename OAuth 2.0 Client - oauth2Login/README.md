a6b43ca742456340ec8ab97a295790c48d046be8
Junseong kim <anima94@kakao.com>
2022-11-08 10:59:31 +0900

    OAuth2LoginConfigurer 초기화 이해

---
+++ OAuth 2.0 Client - oauth2Login/README.md
@@ -0,0 +1,30 @@
+# OAuth 2.0 Client - oauth2Login()
+
+## OAuth2LoginConfigurer 초기화 이해
+
+### 설정 클래스 생성
+
+```java
+@Configuration(proxyBeanMethods = false)
+public class CustomOAuth2ClientConfig {
+
+    @Bean
+    SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Client();
+        return http.build();
+    }
+}
+
+```
+
+- API 설정
+  - SecurityFilterChain 타입의 빈을 생성해서 보안 필터를 구성한다
+  - HttpSecurity에 있는 oauth2Login()과 oauth2Client() API를 정의하고 빌드한다
+
+<img width="1326" alt="image" src="https://user-images.githubusercontent.com/40031858/200452979-720a787c-f49a-49f0-aa1b-3412cfcbe2fa.png">
+
+<img width="1333" alt="image" src="https://user-images.githubusercontent.com/40031858/200453015-6886e64e-6152-474c-90ba-e73c80331816.png">
+
+<img width="1376" alt="image" src="https://user-images.githubusercontent.com/40031858/200453075-c21ffe79-f84c-4dfb-afc5-fe0666067ea7.png">
--- SpringSecurityOAuth2Client/src/main/kotlin/me/saechimdaeki/springsecurityoauth2/OAuth2ClientConfig.kt
+++ SpringSecurityOAuth2Client/src/main/kotlin/me/saechimdaeki/springsecurityoauth2/OAuth2ClientConfig.kt
@@ -2,11 +2,15 @@ package me.saechimdaeki.springsecurityoauth2
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.oauth2.client.registration.ClientRegistration
 import org.springframework.security.oauth2.client.registration.ClientRegistrations
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class OAuth2ClientConfig {
 
     @Bean
@@ -19,5 +23,12 @@ class OAuth2ClientConfig {
             .clientSecret("IUcUxC5no8zlGo7aWGLzdBguo92BWCV3")
             .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
             .build()
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity) : SecurityFilterChain {
+        http.authorizeRequests().anyRequest().authenticated()
+        http.oauth2Login()
+        return http.build()
     }
 }
