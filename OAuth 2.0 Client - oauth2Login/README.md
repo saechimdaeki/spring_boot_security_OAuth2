d54e5d04f6cba3b9f1761d89f44c5a584664b6ec
Junseong kim <anima94@kakao.com>
2022-11-11 11:32:32 +0900

    OAuth2 로그인 구현 - Oauth 2.0 User 모델

--- OAuth 2.0 Client - oauth2Login/README.md
+++ OAuth 2.0 Client - oauth2Login/README.md
@@ -125,6 +125,66 @@ public class CustomOAuth2ClientConfig {
 <img width="1157" alt="image" src="https://user-images.githubusercontent.com/40031858/200714596-309cae53-3afe-434e-864b-7466259127a6.png">
 
 
+---
+
+# OAuth 2.0 User 모델 소개
+
+## OAuth2UserService
+
+### 개요
+
+- OAuth2UserService
+  - 액세스 토큰을 사용해서 UserInfo 엔드포인트 요청으로 최종 사용자의 (리소스 소유자) 속성을 가져오며 OAuth2User 타입의 객체를 리턴한다
+  - 구현체로 DefaultOAuth2UserService 와 OidcUserService 가 제공된다
+
+
+  - DefaultOAuth2UserService
+    - 표준 OAuth 2.0 Provider를 지원하는 OAuth2UserService 구현체다
+    - OAuth2UserRequest 에 Access Token 을 담아 인가서버와 통신 후 사용자의 속성을 가지고 온다
+    - 최종 OAuth2User 타입의 객체를 반환한다
+
+  - OidcUserService
+    - OpenID Connect 1.0 Provider를 지원하는 OAuth2UserService 구현체다
+    - OidcUserRequest 에 있는 ID Token 을 통해 인증 처리를 하며 필요시 DefaultOAuth2UserService 를 사용해서 UserInfo 엔드포인트의 사용자 속성을 요청한다
+    - 최종 OidcUser 타입의 객체를 반환한다
+
+<img width="1125" alt="image" src="https://user-images.githubusercontent.com/40031858/201246022-26edd14e-d3a2-42c8-83cf-527f9f1d7dee.png">
+
+<img width="1122" alt="image" src="https://user-images.githubusercontent.com/40031858/201246054-2c86efbf-8f65-4e97-bfb5-8b51ff3b4398.png">
+
+
+## OAuth2User & OidcUser
+
+### 개요
+
+- 시큐리티는 UserAttributes 및 ID Token Claims 을 집계 & 구성하여 OAuth2User 와 OidcUser 타입의 클래스를 제공한다
+
+- OAuth2User
+  - OAuth 2.0 Provider 에 연결된 사용자 주체를 나타낸다
+  - 최종 사용자의 인증에 대한 정보인 Attributes 를 포함하고 있으며 first name, middle name, last name, email, phone number, address 등으로 구성된다
+  - 기본 구현체는 DefaultOAuth2User 이며 인증 이후 Authentication 의 principal 속성에 저장된다
+- OidcUser
+  - OAuth2User 를 상속한 인터페이스이며 OIDC Provider 에 연결된 사용자 주체를 나타낸다
+  - 최종 사용자의 인증에 대한 정보인 Claims 를 포함하고 있으며 OidcIdToken 및 OidcUserInfo 에서 집계 및 구성된다
+  - 기본 구현체는 DefaultOidcUser 이며 DefaultOAuth2User 를 상속하고 있으며 인증 이후 Authentication 의 principal 속성에 
+     저장된다 
+
+
+<img width="1073" alt="image" src="https://user-images.githubusercontent.com/40031858/201247511-7153a851-6ce3-49f1-811b-9e8247dafa8b.png">
+
+<img width="1149" alt="image" src="https://user-images.githubusercontent.com/40031858/201247538-bfac5bdb-8e5f-4c99-ad57-c2ca925c3e49.png">
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- SpringSecurityOAuth2Client/src/main/kotlin/me/saechimdaeki/springsecurityoauth2/IndexController.kt
+++ SpringSecurityOAuth2Client/src/main/kotlin/me/saechimdaeki/springsecurityoauth2/IndexController.kt
@@ -2,9 +2,18 @@ package me.saechimdaeki.springsecurityoauth2
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
+import org.springframework.security.oauth2.core.OAuth2AccessToken
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames
+import org.springframework.security.oauth2.core.oidc.OidcIdToken
+import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
 
 @RestController
 class IndexController(
@@ -22,5 +31,46 @@ class IndexController(
         val redirectUri = clientRegistration.redirectUri
         logger.info("redirectUri = {}",redirectUri)
         return "index"
+    }
+
+
+    @GetMapping("/user")
+    fun user(accessToken: String) : OAuth2User {
+        val clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak")
+
+        val oAuth2AccessToken =
+            OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, accessToken, Instant.now(), Instant.MAX)
+
+        val oAuth2UserRequest = OAuth2UserRequest(clientRegistration, oAuth2AccessToken)
+
+        val defaultOAuth2UserService = DefaultOAuth2UserService()
+
+        val oAuth2User = defaultOAuth2UserService.loadUser(oAuth2UserRequest)
+
+        return oAuth2User
+    }
+
+
+
+    @GetMapping("/oidc")
+    fun oidc(accessToken: String, idToken: String) : OAuth2User {
+        val clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak")
+
+        val oAuth2AccessToken =
+            OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, accessToken, Instant.now(), Instant.MAX)
+
+        val idTokenClaims = HashMap<String,Any>()
+
+        idTokenClaims.put(IdTokenClaimNames.ISS, "http://localhost:8080/realms/oauth2")
+        idTokenClaims.put(IdTokenClaimNames.SUB,"OIDC0")
+        idTokenClaims.put("preferred_username","user")
+
+        val oidcIdToken = OidcIdToken(idToken, Instant.now(), Instant.MAX, idTokenClaims)
+
+        val oAuth2UserRequest = OidcUserRequest(clientRegistration, oAuth2AccessToken, oidcIdToken)
+        val oidcUserService = OidcUserService()
+        val oAuth2User = oidcUserService.loadUser(oAuth2UserRequest)
+
+        return oAuth2User
     }
 }
