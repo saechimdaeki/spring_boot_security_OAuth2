83f20e7d8b27574a1f7cbe0b94c278ab57e535da
Junseong kim <anima94@kakao.com>
2022-11-16 19:34:08 +0900

    OAuth2ClientConfigurer 초기화 이해

---
+++ OAuth 2.0 Client - oauth2Client()/README.md
@@ -0,0 +1,7 @@
+# OAuth 2.0 Client - oauth2Client()
+
+## OAuth2ClientConfigurer 초기화 이해
+
+<img width="1411" alt="image" src="https://user-images.githubusercontent.com/40031858/202156064-80ae8009-aed8-41ea-994a-d34dc395d6b9.png">
+
+<img width="1437" alt="image" src="https://user-images.githubusercontent.com/40031858/202156193-94549c0c-a62c-45f6-bf37-4f828a136a8d.png">
--- SpringSecurityOAuth2Client/src/main/kotlin/me/saechimdaeki/springsecurityoauth2/OAuth2ClientConfig.kt
+++ SpringSecurityOAuth2Client/src/main/kotlin/me/saechimdaeki/springsecurityoauth2/OAuth2ClientConfig.kt
@@ -2,6 +2,7 @@ package me.saechimdaeki.springsecurityoauth2
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
@@ -14,47 +15,37 @@ class OAuth2ClientConfig(
     private val clientRegistrationRepository: ClientRegistrationRepository,
 ) {
 
-
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.authorizeRequests { authRequest ->
-            authRequest.antMatchers("/home").permitAll()
-                .anyRequest().authenticated()
-        }
+    fun oauth2SecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeRequests { authRequest -> authRequest.anyRequest().authenticated() }
+            .oauth2Login(Customizer.withDefaults())
+            .oauth2Client(Customizer.withDefaults())
 
-        http.oauth2Login { authLogin ->
-            authLogin.authorizationEndpoint { authEndpoint ->
-                authEndpoint.authorizationRequestResolver(customOAuth2AuthorizationRequestResolver())
-            }
-        }
-
-        http.logout().logoutSuccessUrl("/home")
         return http.build()
     }
 
-    private fun customOAuth2AuthorizationRequestResolver(): OAuth2AuthorizationRequestResolver? {
-        return CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization")
-    }
 
 //    @Bean
-//    fun oauth2SecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
-//        http.authorizeRequests { request ->
-//            request.antMatchers("/login","/home").permitAll()
+//    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+//        http.authorizeRequests { authRequest ->
+//            authRequest.antMatchers("/home").permitAll()
 //                .anyRequest().authenticated()
 //        }
-//        http.oauth2Login { oauth2 ->
-//            oauth2.loginPage("/login")
-//                .loginProcessingUrl("/login/v2/oauth2/code/*")
-//                .authorizationEndpoint { authorizationEndpointConfig ->
-//                    authorizationEndpointConfig.baseUri("/oauth2/v1/authorization")
-//                }
-//                .redirectionEndpoint { redirectionEndPointConfig ->
-//                    redirectionEndPointConfig.baseUri("/login/v1/oauth2/code/*")
-//                }
+//
+//        http.oauth2Login { authLogin ->
+//            authLogin.authorizationEndpoint { authEndpoint ->
+//                authEndpoint.authorizationRequestResolver(customOAuth2AuthorizationRequestResolver())
+//            }
 //        }
 //
+//        http.logout().logoutSuccessUrl("/home")
 //        return http.build()
 //    }
+//
+//    private fun customOAuth2AuthorizationRequestResolver(): OAuth2AuthorizationRequestResolver? {
+//        return CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization")
+//    }
+
 
 
 //
