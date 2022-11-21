5f697499b110cfabda1bbd649aed5488ca1af9c7
Junseong kim <anima94@kakao.com>
2022-11-21 14:45:39 +0900

    DefaultOAuth2AuthorizedClientManager - Resource Owner Password 권한 부여 authorizedClient

--- SpringSecurityOAuth2Client/src/main/kotlin/me/saechimdaeki/springsecurityoauth2/oauth2Client/controller/Oauth2LoginController.kt
+++ SpringSecurityOAuth2Client/src/main/kotlin/me/saechimdaeki/springsecurityoauth2/oauth2Client/controller/Oauth2LoginController.kt
@@ -1,8 +1,12 @@
 package me.saechimdaeki.springsecurityoauth2.oauth2Client.controller
 
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
@@ -31,8 +35,23 @@ class Oauth2LoginController(
 
         val authorizedClient = oAuth2AuthorizedClientManager.authorize(authorizeRequest)
 
-        if(authorizedClient != null) {
+        authorizedClient?.let {
+            val oAuth2UserService = DefaultOAuth2UserService()
+            val clientRegistration = authorizedClient.clientRegistration
+            val accessToken = authorizedClient.accessToken
+            val oAuth2UserRequest = OAuth2UserRequest(clientRegistration, accessToken)
+            val oAuth2User = oAuth2UserService.loadUser(oAuth2UserRequest)
 
+            val authorityMapper = SimpleAuthorityMapper()
+            authorityMapper.setPrefix("SYSTEM_")
+            val grantedAuthorities = authorityMapper.mapAuthorities(oAuth2User.authorities)
+
+            val oAuth2AuthenticationToken =
+                OAuth2AuthenticationToken(oAuth2User, grantedAuthorities, clientRegistration.registrationId)
+
+            SecurityContextHolder.getContext().authentication = oAuth2AuthenticationToken
+
+            model.addAttribute("oAuth2AuthenticationToken", oAuth2AuthenticationToken)
         }
 
 
